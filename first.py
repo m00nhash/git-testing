3576ab99697da4d519489d51da20c901581f4ef8
anshul <anshul@ronin.localdomain>
2026-01-20 16:24:48 +0530

    added testing files

---
+++ first.py
@@ -0,0 +1,20 @@
+name = "devops"
+count = 5
+is_active = True
+
+services = []
+
+services = ["api", "ip", "port"]
+
+services.append("hello")
+
+print(services[3])
+
+server = {
+    "website" : "youtube.com",
+    "port" : 80,
+    "state" : "It is active"
+}
+
+
+print(f"{server['website']}\n{server['port']}")
